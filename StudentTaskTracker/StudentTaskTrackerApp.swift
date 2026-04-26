8ed8c02f4e55fa7d186da39d593eb2a3eea8d3bd
Yoan-Stanislav <slaviminchev2@gmail.com>
2026-04-26 17:38:31 +0300

    Initial Commit

---
+++ StudentTaskTracker/StudentTaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentTaskTrackerApp.swift
+//  StudentTaskTracker
+//
+//  Created by Yoan-Stanislav on 26.04.26.
+//
+
+import SwiftUI
+
+@main
+struct StudentTaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
