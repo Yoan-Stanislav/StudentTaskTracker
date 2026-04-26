e2d5456067e606c36a98bec774c09398e9e53fc2
Yoan-Stanislav <slaviminchev2@gmail.com>
2026-04-26 18:42:59 +0300

    add new edit function

---
+++ StudentTaskTracker/EditTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskView.swift
+//  StudentTaskTracker
+//
+//  Created by Yoan-Stanislav on 26.04.26.
+//
+
+import SwiftUI
+
+struct EditTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTaskView()
+}
